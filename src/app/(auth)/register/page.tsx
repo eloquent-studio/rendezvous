e64f83df8ebdb39ee8c6b87cd59d75385feffe2b
fvdime <73942727+fvdime@users.noreply.github.com>
2024-05-08 16:20:15 +0300

    refactor auth

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "@/components/auth-forms/register-form";
 export default function Register({ searchParams }: { searchParams?: { next: string | undefined } }) {
   const next = searchParams?.next || ""
   return (
-    <div className='h-screen w-screen'>
+    <div className='w-full h-full'>
       <RegisterForm next={next} />
     </div>
   )
